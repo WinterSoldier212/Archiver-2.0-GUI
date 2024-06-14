98b3a3ee056da4d1debe51e41a5c796b0b44a384
WinterSoldier212 <04.12.2006mmm@gmail.com>
2024-06-15 01:26:01 +0300

    Textbox 1.0
    Почти идеальный класс для поля ввода текста.
    Нужен шрифт, с ровными по ширине символами.
    Пример использования в конце файла.

--- GUI_Archiver/Textbox.h
+++ GUI_Archiver/Textbox.h
@@ -1,6 +1,7 @@
 #pragma once
 #pragma warning(disable : 4996)
 #include <SFML/Graphics.hpp>
+#include <stack>
 
 using namespace std;
 
@@ -10,60 +11,72 @@ class TextBox
     sf::RectangleShape box;
     sf::Clock clock;
 
-    string inputText;
+    std::stack<char> inputText;
     string visibleText;
-
-    int characterLimit = 255;
-    int visibleCharacterLimit;
-
+    int characterLimit{ 255 };
+    int visibleCharacterLimit{ 0 };
     bool isFocused = false;
 public:
-    TextBox() = delete;
-    TextBox(TextBox& textBox) = delete;
+    TextBox() = default;
+    TextBox(TextBox& textBox) = default;
 
-    TextBox(int size_, sf::Color color_)
+    void SetFontText(sf::Font& font_) { text.setFont(font_); }
+    void SetCharacterSizeText(int size_)
     {
         text.setCharacterSize(size_);
-        text.setColor(color_);
-
-        box.setSize(sf::Vector2f(visibleCharacterLimit * size_ * 5, size_ * 1.5));
-        box.setFillColor(sf::Color::White);
-        box.setOutlineColor(sf::Color::Black);
-        box.setOutlineThickness(2);
+        ReSizeBox();
     }
-    void SetFont(sf::Font& fontForText) { text.setFont(fontForText); }
-    void SetCharacterLimit(int limit_) { characterLimit = limit_; }
-    void SetVisibleCharacterLimit(int visibleLimit_) { visibleCharacterLimit = visibleLimit_; }
-
-    void SetCharacterSize(int size_) { text.setCharacterSize(size_); }
-    void SetVisibleCharacterLimit(int limit) { visibleCharacterLimit = limit; }
-
-    void SetColorForText(sf::Color& colorForText) { text.setColor(colorForText); }
-    void SetColorsForBackgrounds(sf::Color& fillColor, sf::Color& outlineColor)
+    void SetVisibleCharacterLimit(int visibleLimit_)
+    {
+        visibleCharacterLimit = visibleLimit_;
+        ReSizeBox();
+    }
+    void SetColorText(sf::Color& color_) { text.setColor(color_); }
+    void SetOutlineThickness(int outlineThickness_) { box.setOutlineThickness(outlineThickness_); }
+    void SetColorForOutline(sf::Color& outlineColor) { box.setOutlineColor(outlineColor); }
+    void SetColorForFill(sf::Color& fillColor) { box.setFillColor(fillColor); }
+    void SetColorsForBackground(sf::Color& fillColor, sf::Color& outlineColor)
     {
         box.setFillColor(fillColor);
         box.setOutlineColor(outlineColor);
+    }
+    void SetCharacterLimit(int limit_) { characterLimit = limit_; }
+    void SetPosition(sf::Vector2f position_)
+    {
+        text.setPosition(position_);
+        box.setPosition(position_);
     }
 
     void Update(sf::RenderWindow& window, sf::Event& event)
     {
         if (isFocused)
         {
-            if (event.type == sf::Event::TextEntered)
+            if (event.type == sf::Event::TextEntered && inputText.size() != characterLimit)
             {
                 if (std::isprint(event.text.unicode))
-                    inputText += event.text.unicode;
+                    inputText.push(event.text.unicode);
             }
             else if (event.type == sf::Event::KeyPressed)
             {
                 if (event.key.code == sf::Keyboard::BackSpace && !inputText.empty())
                 {
-                    inputText.pop_back();
+                    inputText.pop();
                 }
-                if (event.key.code == sf::Keyboard::Return)
-                {
-                    inputText += '\n';
-                }
+            }
+
+            stack<char> copyInputText = inputText;
+            stack<char> reverseInputText;
+            for (size_t i = 0; i < visibleCharacterLimit && !copyInputText.empty(); ++i)
+            {
+                reverseInputText.push(copyInputText.top());
+                copyInputText.pop();
+            }
+
+            visibleText = "";
+            while (!reverseInputText.empty())
+            {
+                visibleText += reverseInputText.top();
+                reverseInputText.pop();
             }
         }
         if (event.type == sf::Event::MouseButtonPressed)
@@ -80,25 +93,25 @@ public:
                 isFocused = false;
             }
         }
-
+    }
+    void Tick()
+    {
         static sf::Time text_effect_time;
         static bool show_cursor;
 
+        if (!isFocused)
+        {
+            text.setString(visibleText);
+            return;
+        }
+        
         text_effect_time += clock.restart();
-
         if (text_effect_time >= sf::seconds(0.5f))
         {
             show_cursor = !show_cursor;
             text_effect_time = sf::Time::Zero;
         }
-
-        text.setString(inputText + (show_cursor ? '_' : ' '));
-    }
-
-    void SetPosition(sf::Vector2f point)
-    {
-        text.setPosition({ point.x + text.getCharacterSize() * 0.2, point.y + text.getCharacterSize() * 0.2 });
-        box.setPosition(point);
+        text.setString(visibleText + (show_cursor ? '_' : ' '));
     }
 
     void DrawTo(sf::RenderWindow& window)
@@ -106,4 +119,38 @@ public:
         window.draw(box);
         window.draw(text);
     }
+
+private:
+    void ReSizeBox()
+    {
+        int text_lenght = visibleCharacterLimit;
+        int text_size = text.getCharacterSize();
+
+        box.setSize(sf::Vector2f(text_size * ((text_lenght) / 2 + 1), text_size + (text_size * 0.3)));
+        // text_size + (text_size * 0.3) = y
+        // text_size * (text_lenght + 1) / 2 = x
+    }
 };
+/*
+    sf::Color white = sf::Color::White;
+    sf::Color black = sf::Color::Black;
+
+    Textbox textbox;
+    textbox.SetCharacterLimit(30);
+    textbox.SetCharacterSizeText(25);
+    textbox.SetColorForFill(white);
+    textbox.SetColorForOutline(black);
+    textbox.SetColorsForBackground(white, black);
+    textbox.SetColorText(black);
+    textbox.SetFontText(font);
+    textbox.SetOutlineThickness(2);
+    textbox.SetPosition(sf::Vector2f(150, 100));
+    textbox.SetVisibleCharacterLimit(30);
+
+    while:
+        while event:
+            textbox.Update(window, Event);
+        
+        textbox.Tick();
+        textbox.DrawTo(window);
+*/
